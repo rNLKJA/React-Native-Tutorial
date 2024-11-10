8a81a30879a6692075a973e75cd3a051f03ab4dd
Sunchuangyu Huang <62606765+rNLKJA@users.noreply.github.com>
2024-11-10 13:50:38 +1100

    updated

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "../global.css";
+
+export default function App() {
+	return (
+		<View className="flex-1 items-center justify-center bg-white">
+			<Text className="text-blue-500">
+				Open up App.js to start working on your app!
+			</Text>
+		</View>
+	);
+}
