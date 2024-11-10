547968829bfc3e1798f396106e147a6a29517f95
Sunchuangyu Huang <62606765+rNLKJA@users.noreply.github.com>
2024-11-10 12:24:27 +1100

    start tailwindcss

--- auro/app/_layout.tsx
+++ auro/app/_layout.tsx
@@ -1,11 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Slot } from "expo-router";
+import { Stack } from "expo-router";
 
 const RootLayout = () => {
 	return (
-		<View style={styles.container}>
-			<Text>RootLayout</Text>
-		</View>
+		// <View style={styles.container}>
+		// 	<Text>RootLayout</Text>
+		// </View>
+		// <>
+		// 	<Text>Header</Text>
+		// 	<Slot />
+		// 	<Text>Footer</Text>
+		// </>
+		<Stack>
+			<Stack.Screen name="index" options={{ headerShown: false }} />
+		</Stack>
 	);
 };
 
--- auro/app/index.tsx
+++ auro/app/index.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function App() {
 	return (
 		<View style={styles.container}>
 			<Text>Auro</Text>
 			<StatusBar style="auto" />
+			<Link href="/profile" style={{ color: "blue" }}>
+				Go to Profile
+			</Link>
 		</View>
 	);
 }
